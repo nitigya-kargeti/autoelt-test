2be5e29cc141e179cb0947c6ef8b8f154d8fd592
Nitigya <nitigya.kargeti@dataplatr.com>
2026-01-13 16:06:03 -0600

    Delete snf_aelt directory

--- snf_aelt/macros/assert_metadata_exists.sql
+++
@@ -1,8 +0,0 @@
-{% macro assert_metadata_exists() %}
-  {% if execute %}
-    {% set model_node = graph.nodes.values() | selectattr('unique_id', 'equalto', model.unique_id) | first %}
-    {% if not model_node.description %}
-      {{ exceptions.raise_compiler_error("FATAL: Metadata description is missing for model '" ~ model.name ~ "'. The corresponding .yml file entry is required.") }}
-    {% endif %}
-  {% endif %}
-{% endmacro %}
